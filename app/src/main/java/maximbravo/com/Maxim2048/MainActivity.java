ea32d15e1e5db41f006c73f4aecb1f17b77d0c5a
Maxim Bravo <maxim@alexbravo.com>
2016-10-18 08:40:42 -0700

    Add portrait control

--- app/src/main/java/maximbravo/com/Maxim2048/MainActivity.java
+++ app/src/main/java/maximbravo/com/Maxim2048/MainActivity.java
@@ -1,6 +1,7 @@
 package maximbravo.com.Maxim2048;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Bundle extras = getIntent().getExtras();
         if(extras != null){
             b.convertIntArrayToBoard(0, extras.getIntArray("row1"));
